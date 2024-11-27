166e82082277645f72b728cff300d265dc4fe13d
claudiobzrr <claudio.bezerra1998@gmail.com>
2024-11-27 00:34:52 -0300

    ✔ Add: align icons

--- src/components/action-button.tsx
+++ src/components/action-button.tsx
@@ -1,11 +1,13 @@
-import { Icon, Stack} from "@chakra-ui/react";
+import { Icon, Stack, Text, VStack} from "@chakra-ui/react";
 import React from "react";
 
 export function ActionButton({onAction, text, icon}: {onAction: () => void, text:string, icon: React.ReactElement}){
     return(
-        <Stack w={60} h={60} _hover={{bg:'blue.100', transition: 'all 0.2s ease-in-out', cursor: 'pointer'}} gap={0} align={'center'} justify={'center'} p={12} borderWidth={1} rounded={'full'}  onClick={onAction}>
-            <Icon w={{base:16,md:32}} h={{base:16,md:32}}>{icon}</Icon>
-            {text}
-        </Stack>
+        <VStack gap={0}>
+            <Stack textAlign={'center'}  fontSize={{base: 'md', md: 'lg'}} css={{fontFamily: 'Sour Gummy'}} w={{base:24,md:72}} h={{base:24,md:72}} _hover={{bg:'blue.100', transition: 'all 0.2s ease-in-out', cursor: 'pointer'}} gap={0} align={'center'} justify={'center'} p={2} borderWidth={1} rounded={'full'}  onClick={onAction}>
+                <Icon w={{base:16,md:32}} h={{base:16,md:32}}>{icon}</Icon>
+            </Stack>
+            <Text fontSize={{base: 'md', md: '2xl'}} css={{fontFamily: 'Sour Gummy'}}>{text}</Text>
+        </VStack>
     )
 }
--- src/components/sections/actions.tsx
+++ src/components/sections/actions.tsx
@@ -6,14 +6,14 @@ import { LuGift } from "react-icons/lu";
 
 export function Actions() {
     return (
-        <VStack w={'100%'} minH={'100vh'}>
+        <VStack align={'center'} justify={'center'} w={'full'} minH={'100vh'}>
             <Text textAlign={'center'} css={{fontFamily: 'Authentic Signature', color: 'blue.400',}} fontSize={{base: '2xl', md: '7xl'}}>Clíque nos ícones para acessar</Text>
-            <VStack w={'full'} h={'full'} gap={5}>
-                <HStack w={'full'} h={'full'}>
+            <VStack align={'center'} justify={'center'}  w={'full'} h={'full'} gap={{base:5,md:12}}>
+                <HStack align={'center'} justify={'center'} gap={{base:5,md:12}} w={'full'} h={'full'}>
                     <ActionButton onAction={() => {}} icon={<IoLocationOutline/>} text={'Localização da cerimônia'} />
                     <ActionButton onAction={() => {}} icon={<IoLocationOutline/>} text={'Localização do evento'} />
                 </HStack>
-                <HStack w={'full'} h={'full'}>
+                <HStack align={'center'} justify={'center'} gap={{base:5,md:12}} w={'full'} h={'full'}>
                     <ActionButton onAction={() => {}} icon={<FiCheckCircle/>} text={'Confirme sua presença'} />
                     <ActionButton onAction={() => {}} icon={<LuGift/>} text={'Presente'} />
                 </HStack>
