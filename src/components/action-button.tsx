0163db19f6a2e28cc33afdbabe854151921f8c2c
claudiobzrr <claudio.bezerra1998@gmail.com>
2024-11-27 21:51:55 -0300

    ✔ Add: style to action buttons

--- src/components/action-button.tsx
+++ src/components/action-button.tsx
@@ -4,8 +4,8 @@ import React from "react";
 export function ActionButton({onAction, text, icon}: {onAction: () => void, text:string, icon: React.ReactElement}){
     return(
         <VStack gap={0}>
-            <Stack textAlign={'center'}  fontSize={{base: 'md', md: 'lg'}} css={{fontFamily: 'Sour Gummy'}} w={{base:24,md:72}} h={{base:24,md:72}} _hover={{bg:'blue.100', transition: 'all 0.2s ease-in-out', cursor: 'pointer'}} gap={0} align={'center'} justify={'center'} p={2} borderWidth={1} rounded={'full'}  onClick={onAction}>
-                <Icon w={{base:16,md:32}} h={{base:16,md:32}}>{icon}</Icon>
+            <Stack transition={'all 0.4s ease-in-out'} borderColor={'#736C5F'} bg={'blue.100'} textAlign={'center'}  fontSize={{base: 'md', md: 'lg'}} css={{fontFamily: 'Sour Gummy'}} w={{base:24,md:60}} h={{base:24,md:60}} _hover={{transition: 'all 0.4s ease-in-out', cursor: 'pointer',transform: 'scale(1.02)'}} gap={0} align={'center'} justify={'center'} p={2} borderWidth={4} rounded={'full'}  onClick={onAction}>
+                <Icon color={'#736C5F'} w={{base:16,md:32}} h={{base:16,md:32}}>{icon}</Icon>
             </Stack>
             <Text fontSize={{base: 'md', md: '2xl'}} css={{fontFamily: 'Sour Gummy'}}>{text}</Text>
         </VStack>
--- src/components/sections/actions.tsx
+++ src/components/sections/actions.tsx
@@ -1,20 +1,26 @@
 import { HStack, Text, VStack } from "@chakra-ui/react";
 import { ActionButton } from "../action-button";
-import { IoLocationOutline } from "react-icons/io5";
+// import { IoLocationOutline } from "react-icons/io5";
+import { PiMapPinAreaBold } from "react-icons/pi";
 import { FiCheckCircle } from "react-icons/fi";
 import { LuGift } from "react-icons/lu";
 
 export function Actions() {
+    const confirmationMessage:string ='Olá, gostaria de confirmar minha presença na sua cerimônia de casamento do dia 01/02/2024.'
+    const encondedMessage:string = encodeURIComponent(confirmationMessage)
+    const whatsappLink = `https://api.whatsapp.com/send?phone=5585989349171&text=${encondedMessage}`
+    const locationChurch:string = 'https://maps.app.goo.gl/2nKQTGgGkzayKR9s5'
+    const locationLunch:string = 'https://maps.app.goo.gl/SXWuSuMKaAx29KdVA'
     return (
         <VStack align={'center'} justify={'center'} w={'full'} minH={'100vh'}>
             <Text textAlign={'center'} css={{fontFamily: 'Authentic Signature', color: 'blue.400',}} fontSize={{base: '2xl', md: '7xl'}}>Clíque nos ícones para acessar</Text>
             <VStack align={'center'} justify={'center'}  w={'full'} h={'full'} gap={{base:5,md:12}}>
                 <HStack align={'center'} justify={'center'} gap={{base:5,md:12}} w={'full'} h={'full'}>
-                    <ActionButton onAction={() => {}} icon={<IoLocationOutline/>} text={'Localização da cerimônia'} />
-                    <ActionButton onAction={() => {}} icon={<IoLocationOutline/>} text={'Localização do evento'} />
+                    <ActionButton onAction={() => {window.open(locationChurch,'_blank')}} icon={<PiMapPinAreaBold/>} text={'Localização da cerimônia'} />
+                    <ActionButton onAction={() => {window.open(locationLunch,'_blank')}} icon={<PiMapPinAreaBold/>} text={'Localização do evento'} />
                 </HStack>
                 <HStack align={'center'} justify={'center'} gap={{base:5,md:12}} w={'full'} h={'full'}>
-                    <ActionButton onAction={() => {}} icon={<FiCheckCircle/>} text={'Confirme sua presença'} />
+                    <ActionButton onAction={() => { window.open(whatsappLink,'_blank')}} icon={<FiCheckCircle/>} text={'Confirme sua presença'} />
                     <ActionButton onAction={() => {}} icon={<LuGift/>} text={'Presente'} />
                 </HStack>
             </VStack>
