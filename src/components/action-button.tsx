03f5efce0a02f3de1d58310df4cf553fa1c76927
claudiobzrr <claudio.bezerra1998@gmail.com>
2024-11-27 22:21:39 -0300

    ✔ Renew: action button collor pallete

--- src/components/action-button.tsx
+++ src/components/action-button.tsx
@@ -4,8 +4,8 @@ import React from "react";
 export function ActionButton({onAction, text, icon}: {onAction: () => void, text:string, icon: React.ReactElement}){
     return(
         <VStack gap={0}>
-            <Stack transition={'all 0.4s ease-in-out'} borderColor={'#736C5F'} bg={'blue.200'} textAlign={'center'}  fontSize={{base: 'md', md: 'lg'}} css={{fontFamily: 'Sour Gummy'}} w={{base:24,md:60}} h={{base:24,md:60}} _hover={{transition: 'all 0.4s ease-in-out', cursor: 'pointer',transform: 'scale(1.02)'}} gap={0} align={'center'} justify={'center'} p={2} borderWidth={4} rounded={'full'}  onClick={onAction}>
-                <Icon color={'#736C5F'} w={{base:16,md:32}} h={{base:16,md:32}}>{icon}</Icon>
+            <Stack transition={'all 0.4s ease-in-out'} borderColor={'#736C5F'} bg={'blue.400'} textAlign={'center'}  fontSize={{base: 'md', md: 'lg'}} css={{fontFamily: 'Sour Gummy'}} w={{base:24,md:60}} h={{base:24,md:60}} _hover={{transition: 'all 0.4s ease-in-out', cursor: 'pointer',transform: 'scale(1.02)'}} gap={0} align={'center'} justify={'center'} p={2} borderWidth={4} rounded={'full'}  onClick={onAction}>
+                <Icon color={'white'} w={{base:16,md:32}} h={{base:16,md:32}}>{icon}</Icon>
             </Stack>
             <Text fontSize={{base: 'md', md: '2xl'}} css={{fontFamily: 'Sour Gummy'}}>{text}</Text>
         </VStack>
